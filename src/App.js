c9b070ad13a3f9913f8d7abcfcb49a9d1be24ae2
I-ShivamSingh <shivamrocks.singh@gmail.com>
2023-09-22 23:25:56 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Header from './components/Header';
+import ServerList from './components/ServerList';
+import ServerForm from './components/ServerForm';
 
 function App() {
+  const [servers, setServers] = useState([]);
+  
+  // Function to add a new server
+  const handleAddServer = (newServer) => {
+    setServers([...servers, newServer]);
+  };
+
+  // Function to delete a server by ID
+  const handleDeleteServer = (serverId) => {
+    const updatedServers = servers.filter((server) => server.id !== serverId);
+    setServers(updatedServers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Header />
+        <ServerForm onSubmit={handleAddServer} />
+        <ServerList servers={servers} onDelete={handleDeleteServer} />
+      </Container>
     </div>
   );
 }
